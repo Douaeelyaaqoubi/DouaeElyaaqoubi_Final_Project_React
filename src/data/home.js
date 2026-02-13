063c53b0837d2f4c1ff40f2d3c84e09b2d9e1a9e
Douae <douaeelyaaqoubi2@gmail.com>
2026-02-13 16:00:53 +0100

    push

---
+++ src/data/home.js
@@ -0,0 +1,55 @@
+import { Img } from "../constants/img"; 
+
+export const homeData = {
+    featuredProducts: [
+        {
+            id: 1,
+            title: "Boxy7 T-Shirt with Roll Sleeve",
+            price: 20,
+            image: Img.shopy8,
+        },
+        {
+            id: 2,
+            title: "Boxy6 T-Shirt with Roll Sleeve",
+            price: 20,
+            image: Img.shopy7,
+        },
+        {
+            id: 3,
+            title: "Boxy5 T-Shirt with Roll Sleeve",
+            price: 20,
+            image: Img.shopy6,
+        },
+        {
+            id: 4,
+            title: "Boxy4 T-Shirt with Roll Sleeve",
+            price: 20,
+            image: Img.shopy5
+        },
+        {
+            id: 5,
+            title: "Boxy3 T-Shirt with Roll Sleeve",
+            price: 20,
+            image: Img.shopy4,
+        },
+        {
+            id: 6,
+            title: "Boxy2 T-Shirt with Roll Sleeve",
+            price: 20,
+            image: Img.shopy3,
+        },
+        {
+            id: 7,
+            title: "Boxy1 T-Shirt with Roll Sleeve",
+            price: 20,
+            image: Img.shopi2,
+        },
+        {
+            id: 8,
+            title: "Boxy T-Shirt with Roll Sleeve Detail",
+            price: 20,
+            image: Img.item3,
+        },
+    ],
+
+};
